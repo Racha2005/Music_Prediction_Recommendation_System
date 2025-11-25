0eff91da8d1d742c344b019f3b6af8e63a72d2cf
Rachana R Tunga <147418029+Racha2005@users.noreply.github.com>
2025-11-26 00:41:45 +0530

    Delete music_prediction_recommendation_analysis_project directory

--- music_prediction_recommendation_analysis_project/music_app/cli.py
+++
@@ -1,5 +0,0 @@
-from music_app.ml.recommender import recommend
-
-if __name__=='__main__':
-    print('Sample recommendations for track_1:')
-    print(recommend('track_1', top_k=5))
--- music_prediction_recommendation_analysis_project/setup.py
+++
@@ -1,2 +0,0 @@
-from setuptools import setup
-setup(name='music_prediction_recommendation_analysis_project', version='0.1')
